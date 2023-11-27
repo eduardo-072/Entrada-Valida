9a5abe547552585096efa9d331a15b7717fa81fd
eduardo-072 <eduardosilvaaa072@gmail.com>
2023-11-27 17:15:31 -0300

    tarefa completa

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("-----Entrada Válida-----");
+ int NumeroDigitado = -1;
+ 
+while (NumeroDigitado < 0 || NumeroDigitado >9)
+{
+    Console.WriteLine("Digite um número de 1 a 9 ou 0 para parar a operação");
+    NumeroDigitado = Convert.ToInt32(Console.ReadLine());     
+}
+
+ if (NumeroDigitado == 0)
+ {
+    Console.WriteLine("Operação foi cancelada pelo usuário");
+ }
+
+ else
+{
+    Console.WriteLine($"Número selecionado = {NumeroDigitado}");
+}
+
+Console.WriteLine("VOLTE SEMPRE !!!");
